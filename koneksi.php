d203121983185eac0343e3cdb5903cccebd5b072
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2023-08-04 23:01:56 +0800

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,3 @@
-<?php 
+<?php
 
-$koneksi = mysqli_connect("localhost", "root", "" ,"arsipdig");
-
-?>
+$koneksi = mysqli_connect("31.170.164.102", "u319221554_arsipdig", "U319221554_arsipdig", "u319221554_arsipdig");
